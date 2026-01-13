7ec70f90f1a13b90465195a70aa41ae304cfd5a8
sulivan cerdan <sulivan.cerdan.etu@univ-lille.fr>
2026-01-13 21:42:29 +0100

    maj cv

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ImNotaJoke.github.io
 Portfolio représentative de l'étudiant
+Ceci est un portfolio pour présenter mes projets et compétences.
--- script/thanks.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Merci pour votre message ! Je vous répondrai dès que possible 😄");
-});
